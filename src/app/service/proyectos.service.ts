4aaccca5c77bf99cc39ec61ebf3718c06acad0f5
sergioeabarcaf <sergioaf1991@gmail.com>
2017-11-13 16:14:44 -0300

    sergioeabarcaf UPDATE: Se elimina bootstrap CDN y se agrega el maximo de caracteres en el parrafo

--- src/app/service/proyectos.service.ts
+++ src/app/service/proyectos.service.ts
@@ -6,7 +6,7 @@ export class ProyectosService {
   private proyectos:Proyecto[] = [
     {
       nombre:"proyecto1",
-      descripcion:"jhakjhakahkjahjkahkajhakja",
+      descripcion:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidid",
       categoria:"Textil",
       imagen1:"http://via.placeholder.com/700x450",
       imagen2:"http://via.placeholder.com/700x450",
@@ -14,11 +14,11 @@ export class ProyectosService {
       imagenGrupal:"http://via.placeholder.com/700x450",
       partner:"Soy el dueño 1",
       partnerIMG:"http://via.placeholder.com/450x450",
-      partnerMSJ:"blajlaklaklaklak"
+      partnerMSJ:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
       nombre:"proyecto2",
-      descripcion:"jhakjhakahkjahjkahkajhakja",
+      descripcion:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       categoria:"iot",
       imagen1:"http://via.placeholder.com/700x450",
       imagen2:"http://via.placeholder.com/700x450",
@@ -30,7 +30,7 @@ export class ProyectosService {
     },
     {
       nombre:"proyecto3",
-      descripcion:"jhakjhakahkjahjkahkajhakja",
+      descripcion:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
       categoria:"SmartCities",
       imagen1:"http://via.placeholder.com/700x450",
       imagen2:"http://via.placeholder.com/700x450",
