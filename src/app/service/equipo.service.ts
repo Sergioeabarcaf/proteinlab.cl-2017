410527f3686310d865c9f7b9a671d01c715a096e
sergioeabarcaf <sergioaf1991@gmail.com>
2018-02-28 18:38:58 -0300

    se agregan descripciones para hector,maca,hugo y victorEscobar

--- src/app/service/equipo.service.ts
+++ src/app/service/equipo.service.ts
@@ -14,7 +14,7 @@ export class EquipoService {
       linkedin: "http://www.linkedin.com/paz"
     },
     {
-      nombre: "Diego Vilchez",
+      nombre: "Diego Vilches",
       area: "Diseño",
       descripcion: "lorenakjahjahkjahahakjhajkahjkahkjahakjhajkah",
       imagen: "http://via.placeholder.com/700x450",
@@ -72,14 +72,28 @@ export class EquipoService {
     {
       nombre: "Macarena Valenzuela",
       area: "Dirección",
-      descripcion: "lorenakjahjahkjahahakjhajkahjkahkjahakjhajkah",
+      descripcion: "Diseñadora Industrial de la Universidad Tecnológica Metropolitana de Chile. El 2011 se especializa en Gestión Estratégica en Diseño y proyectos en la Universidad de Buenos Aires. Sus intereses se enfocan en el mundo del emprendimiento, trabajo colaborativo, proyectos con impacto y el uso de tecnologías como medio para la obtención de resultados.",
       imagen: "http://via.placeholder.com/700x450",
       linkedin: "http://www.linkedin.com/macarena"
     },
     {
       nombre: "Hector Torres",
       area: "Dirección",
-      descripcion: "lorenakjahjahkjahahakjhajkahjkahkjahakjhajkah",
+      descripcion: "Doctor en Gestión del Diseño y TICs por la Universidad Politécnica de Valencia, España y Magister en Diseño Industrial por la Universidad Jaume I, Castellón, España. Ha orientado su actividad de I+D+i en el área de innovación y tecnologías digitales; entornos inteligentes y Smart Cities; y estudio de comunicación entre usuarios y entornos interactivos.",
+      imagen: "http://via.placeholder.com/700x450",
+      linkedin: "http://www.linkedin.com/hector"
+    },
+    {
+      nombre: "Hugo Durney",
+      area: "Dirección",
+      descripcion: "Académico universitario, docente e investigador en las áreas de Ingeniería Electrónica, Telecomunicaciones y Procesamiento Digital de Señales, con más de 15 años de experiencia vinculado al ámbito de educación superior incluyendo docencia, I+D+i y gestión directiva central universitaria.",
+      imagen: "http://via.placeholder.com/700x450",
+      linkedin: "http://www.linkedin.com/hector"
+    },
+    {
+      nombre: "Victor Escobar",
+      area: "Dirección",
+      descripcion: "Académico e investigador en Ingeniería Informática. Doctor en Informática por la Universidad de Granada, dentro del programa “ Diseño, Análisis y Aplicaciones en Sistemas Inteligentes”, España.",
       imagen: "http://via.placeholder.com/700x450",
       linkedin: "http://www.linkedin.com/hector"
     }
