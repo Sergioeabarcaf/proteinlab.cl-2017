d9f6724a7202cc859e2bf5c0e4a9283afd675443
sergioeabarcaf <sergioaf1991@gmail.com>
2018-02-28 13:27:44 -0300

    se agrega useHash=True para no tener problemas al pasar a produccion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,4 +12,4 @@ const APP_ROUTES: Routes = [
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
-export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, { useHash: true });
