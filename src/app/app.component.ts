12bf1bd3d104ebc6692a65eab543540992aa384f
sergioeabarcaf <sergioaf1991@gmail.com>
2018-03-08 15:52:31 -0300

    solucionado problema que no abria la ruta en el top del componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
+import { Location, PopStateEvent } from "@angular/common";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
 
-  onActivate(e, outlet){
-    outlet.scrollTop = 0;
-  }
+  private lastPoppedUrl: string;
+  private yScrollStack: number[] = [];
+
+  constructor(private router: Router, private location: Location) { }
+
+    ngOnInit() {
+        this.location.subscribe((ev:PopStateEvent) => {
+            this.lastPoppedUrl = ev.url;
+        });
+        this.router.events.subscribe((ev:any) => {
+            if (ev instanceof NavigationStart) {
+                if (ev.url != this.lastPoppedUrl)
+                    this.yScrollStack.push(window.scrollY);
+            } else if (ev instanceof NavigationEnd) {
+                if (ev.url == this.lastPoppedUrl) {
+                    this.lastPoppedUrl = undefined;
+                    window.scrollTo(0, this.yScrollStack.pop());
+                } else
+                    window.scrollTo(0, 0);
+            }
+        });
+    }
 }
