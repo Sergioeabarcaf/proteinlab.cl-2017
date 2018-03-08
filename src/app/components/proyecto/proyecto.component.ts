ef81705fc3aa3b898475336b35efc0f2e9eb2706
sergioeabarcaf <sergioaf1991@gmail.com>
2018-03-08 16:59:49 -0300

    eliminacion de servicio equipo

--- src/app/components/proyecto/proyecto.component.ts
+++ src/app/components/proyecto/proyecto.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ProyectosService, Proyecto } from '../../service/proyectos.service';
-import { EquipoService } from '../../service/equipo.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -12,7 +11,7 @@ export class ProyectoComponent {
 
   proyecto:any = {};
 
-  constructor(private activatedRoute:ActivatedRoute, private _proyectoService: ProyectosService, private _equipoService: EquipoService, private route:Router) {
+  constructor(private activatedRoute:ActivatedRoute, private _proyectoService: ProyectosService, private route:Router) {
     this.activatedRoute.params.subscribe( params => {
       this.proyecto = this._proyectoService.getProyecto( params['id']);
     })
