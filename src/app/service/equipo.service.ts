885f43fe1db8222cdfd8d53d92270c5a8690351f
sergioeabarcaf <sergioaf1991@gmail.com>
2018-03-20 12:01:35 -0300

    cambio de foto base

--- src/app/service/equipo.service.ts
+++ src/app/service/equipo.service.ts
@@ -91,7 +91,7 @@ export class EquipoService {
       nombre: "Hector Torres",
       area: "Dirección",
       descripcion: "Doctor en Gestión del Diseño y TICs por la Universidad Politécnica de Valencia, España y Magister en Diseño Industrial por la Universidad Jaume I, Castellón, España. Ha orientado su actividad de I+D+i en el área de innovación y tecnologías digitales; entornos inteligentes y Smart Cities; y estudio de comunicación entre usuarios y entornos interactivos.",
-      imagen: "http://via.placeholder.com/700x450",
+      imagen: "assets/img/Recurso2.svg",
       linkedin: null,
       estado: true
     },
@@ -99,7 +99,7 @@ export class EquipoService {
       nombre: "Hugo Durney",
       area: "Dirección",
       descripcion: "Académico universitario, docente e investigador en las áreas de Ingeniería Electrónica, Telecomunicaciones y Procesamiento Digital de Señales, con más de 15 años de experiencia vinculado al ámbito de educación superior incluyendo docencia, I+D+i y gestión directiva central universitaria.",
-      imagen: "http://via.placeholder.com/700x450",
+      imagen: "assets/img/Recurso2.svg",
       linkedin: null,
       estado: true
     },
@@ -107,7 +107,7 @@ export class EquipoService {
       nombre: "Victor Escobar",
       area: "Dirección",
       descripcion: "Académico e investigador en Ingeniería Informática. Doctor en Informática por la Universidad de Granada, dentro del programa “ Diseño, Análisis y Aplicaciones en Sistemas Inteligentes”, España.",
-      imagen: "http://via.placeholder.com/700x450",
+      imagen: "assets/img/Recurso2.svg",
       linkedin: null,
       estado: true
     }
