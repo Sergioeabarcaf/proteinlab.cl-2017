eaeef8faf7c969bbc1eff33877969d3ed2352504
sergioeabarcaf <sergioaf1991@gmail.com>
2018-03-21 14:59:03 -0300

    cambios de estilos en home y tambien en estructura

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProyectosService, Proyecto } from '../../service/proyectos.service';
+import { EquipoService, Equipo } from '../../service/equipo.service';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +10,13 @@ import { ProyectosService, Proyecto } from '../../service/proyectos.service';
 export class HomeComponent implements OnInit {
 
   proyectosAll:Proyecto[] = [];
+  equipo:Equipo[] = [];
 
-  constructor( private _proyectosService:ProyectosService ) { }
+  constructor( private _proyectosService:ProyectosService, private _equipoService:EquipoService ) { }
 
   ngOnInit() {
     this.proyectosAll = this._proyectosService.getProyectoLimit(3);
+    this.equipo = this._equipoService.getEquipo();
   }
 
 }
